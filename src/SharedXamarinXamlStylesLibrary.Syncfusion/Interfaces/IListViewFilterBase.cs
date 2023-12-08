6a84c53087f947a5e9a9fd35738d9c0f7c0b96d5
Andreas <andy.reitberger@gmail.com>
2023-12-08 08:31:10 +0100

    Created `Syncfusion` project

---
+++ src/SharedXamarinXamlStylesLibrary.Syncfusion/Interfaces/IListViewFilterBase.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.DataSource;
+
+namespace AndreasReitberger.Shared.XForm.Syncfusion.Interfaces
+{
+    public interface IListViewFilterBase
+    {
+        #region Properties
+        public Guid Id { get; set; }
+        public string Identifier { get; set; }
+        public List<SortDescriptor> SortDescriptors { get; set; }
+        public string SearchText { get; set; }
+        public List<IListViewFilterProperty> SearchedPropertiesList { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/SharedXamarinXamlStylesLibrary.Syncfusion/Themes/SharedTemplates.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace AndreasReitberger.Shared.XForm.Syncfusion.Themes
+{
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SharedTemplates
+    {
+		public SharedTemplates()
+		{
+			InitializeComponent ();
+		}
+	}
+}
